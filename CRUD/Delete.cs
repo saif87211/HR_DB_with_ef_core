c651cef7c3ffeccbeded328ab15f7800c3b5927e
Gajdhar Saif Ali <76898897+saif87211@users.noreply.github.com>
2023-07-20 18:02:19 +0000

    Add Delete class for All tables

---
+++ CRUD/Delete.cs
@@ -0,0 +1,83 @@
+using HR_DB_with_ef_core.Data;
+using HR_DB_with_ef_core.Models;
+using static System.Console;
+
+namespace HR_DB_with_ef_core.CRUD;
+
+public class Delete
+{
+    public static void DeleteEmployee(int id)
+    {
+        using (var db = new HRContext())
+        {
+            var employee = db.Employees.First(e => e.EmployeeId == id);
+
+            if (employee != null)
+            {
+                db.Employees.Remove(employee);
+                db.SaveChanges();
+                WriteLine("Data is deleted.\n");
+            }
+            else
+            {
+                WriteLine("Id is not found in employee Table\n");
+            }
+        }
+    }
+
+    public static void DeleteJob(int id)
+    {
+        using (var db = new HRContext())
+        {
+            var job = db.Jobs.Single(j => j.JobId == id);
+
+            if (job != null)
+            {
+                db.Jobs.Remove(job);
+                db.SaveChanges();
+                WriteLine("Data is deleted.\n");
+            }
+            else
+            {
+                WriteLine("Id is not found in job Table\n");
+            }
+        }
+    }
+
+    public static void DeleteDepartment(int id)
+    {
+        using (var db = new HRContext())
+        {
+            var department = db.Departments.Single(d => d.DepartmentId == id);
+
+            if (department != null)
+            {
+                db.Departments.Remove(department);
+                db.SaveChanges();
+                WriteLine("Data is deleted.\n");
+            }
+            else
+            {
+                WriteLine("Id is not found in Department Table\n");
+            }
+        }
+    }
+    public static void DeleteLocation(int id)
+    {
+        using (var db = new HRContext())
+        {
+            var location = db.Locations.Single(l => l.LocationId == id);
+
+            if (location != null)
+            {
+                db.Locations.Remove(location);
+                db.SaveChanges();
+                WriteLine("Data is deleted.\n");
+            }
+            else
+            {
+                WriteLine("Id is not found in Location Table\n");
+            }
+        }
+    }
+}
